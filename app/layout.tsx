80b4d0b409afe2b70cc2252456b4b270d14d1779
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2026-05-24 01:49:15 +0530

    v1.0.5: chore: update favicons - add rounded edges to the favicons - add png favicon for apple devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   keywords: siteMetadata.keywords,
   authors: [{ name: "Prathmesh Ojha" }],
   icons: {
-    icon: [{ url: "/logo.svg", type: "image/svg+xml" }],
+    icon: [{ url: "/favicon.svg", type: "image/svg+xml" }],
     apple: [{ url: "/logo.svg", type: "image/svg+xml" }],
   },
   openGraph: {
